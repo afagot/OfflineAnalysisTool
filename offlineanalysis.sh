4cb586efe815105d37b87d44a9fd494c310b71ae
Alexis Fagot <alexis.fagot@ugent.be>
2015-04-29 12:40:28 +0200

    Fix problem with shell script

--- offlineanalysis.sh
+++ offlineanalysis.sh
@@ -4,14 +4,13 @@ bin/analysis "$1"
 
 sufix='CLUSTERIZED_SORTED_'
 
-test=${1%%\/*}
+path=${1%\/*}
+fName=${1##*\/}
+clusterdata=${1%\/*}"/"$sufix${1##*\/}
 
-if [ $test == ".." ]
-then
-	clusterdata=${1%\/*}"/"$sufix${1##*\/}
-else
-	clusterdata=$sufix$1
-fi
+echo $path
+echo $fName
+echo $clusterdata
 
-root -l -q 'make_histos.cc+("'$clusterdata'",'${2-50000}','${3-35025}','${4-35375}')'
+#root -l -q 'make_histos.cc+("'$clusterdata'",'${2-50000}','${3-35025}','${4-35375}')'
 

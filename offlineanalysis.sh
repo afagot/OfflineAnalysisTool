db00e8a40b9cdff00501587233a4f2b365d06e84
Alexis Fagot <alexis.fagot@ugent.be>
2015-04-29 15:40:34 +0200

    Deleted the commented line in offlineanalysis.sh

--- offlineanalysis.sh
+++ offlineanalysis.sh
@@ -8,9 +8,5 @@ path=${1%\/*}
 fName=${1##*\/}
 clusterdata=${1%\/*}"/"$sufix${1##*\/}
 
-echo $path
-echo $fName
-echo $clusterdata
-
-#root -l -q 'make_histos.cc+("'$clusterdata'",'${2-50000}','${3-35025}','${4-35375}')'
+root -l -q 'make_histos.cc+("'$clusterdata'",'${2-50000}','${3-35025}','${4-35375}')'
 

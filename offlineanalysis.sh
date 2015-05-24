1bb51ff9252d3c27d90ba5cfc43e5044bf388dc8
afagot <alexis.fagot@ugent.be>
2015-05-24 11:17:03 +0200

    Changes in the starting script

--- offlineanalysis.sh
+++ offlineanalysis.sh
@@ -2,11 +2,11 @@
 
 bin/analysis "$1"
 
-sufix='CLUSTERIZED_SORTED_'
+prefix='CLUSTERIZED_SORTED_'
 
 path=${1%\/*}
 fName=${1##*\/}
-clusterdata=${1%\/*}"/"$sufix${1##*\/}
+clusterdata=$path"/"$prefix$fName
 
 root -l -q 'make_histos.cc+("'$clusterdata'",'${2-50000}','${3-35025}','${4-35375}')'
 
